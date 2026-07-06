a15b09cce7c7a8ee4a030b10c955c7309bfc73dc
Juan Lee <me@juany.kr>
2026-07-06 17:30:21 +0900

    Add CloudFront distribution ARN output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "distribution_id" {
   value       = aws_cloudfront_distribution.this.id
 }
 
+output "distribution_arn" {
+  description = "CloudFront distribution ARN"
+  value       = aws_cloudfront_distribution.this.arn
+}
+
 output "domain_name" {
   description = "CloudFront domain name"
   value       = aws_cloudfront_distribution.this.domain_name
@@ -22,6 +27,7 @@ output "module" {
   description = "Full module outputs"
   value = {
     distribution_id          = aws_cloudfront_distribution.this.id
+    distribution_arn         = aws_cloudfront_distribution.this.arn
     domain_name              = aws_cloudfront_distribution.this.domain_name
     origin_access_control_id = local.origin_access_control_id
     acm_certificate_arn      = local.acm_certificate_arn
